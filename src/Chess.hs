77ab6c98be6f2e91d17ef1d07a2f2f00ec4bf919
Robert M. Avram <robert.avram@rewe-digital.com>
2019-03-10 13:43:46 +0100

    Add positions to pieces

--- src/Chess.hs
+++ src/Chess.hs
@@ -15,12 +15,12 @@ type Pos = (Int, Int)
 data Colour = B | W | T deriving (Eq, Show, Ord)
 
 data Piece = 
-    Pawn Int Colour   |
-    Rook Int Colour   |
-    Bishop Int Colour |
-    Knight Int Colour |
-    Queen Colour      |
-    King Colour       |
+    Pawn Colour Pos   |
+    Rook Colour Pos   |
+    Bishop Colour Pos |
+    Knight Colour Pos |
+    Queen Colour Pos  |
+    King Colour Pos   |
     Empty
     deriving (Eq, Ord)
 
@@ -33,13 +33,12 @@ data Move =
     QCastle
     deriving (Show, Eq, Ord)
 
-data Board = Board {positions :: Map Pos Piece, pieces :: Map Piece Pos}
+data Board = Board {positions :: Map Pos Piece,
+                    whiteKing :: Pos,
+                    blackKing :: Pos}
 
 (>?=) :: Monad f => f a -> f b -> f b
 fa >?= fb = fa >>= (const fb)
-
-filterByKey :: (k -> Bool) -> Map k v -> Map k v
-filterByKey p = M.filterWithKey (\k _ -> p k)
 
 kingCastleRookS  = (8, 1)
 queenCastleRookS = (1, 1)
@@ -54,13 +53,30 @@ invert W = B
 invert T = T
 
 colour :: Piece -> Colour
-colour (Pawn _ c) = c
-colour (Rook _ c) = c
-colour (King c) = c
-colour (Queen c) = c
-colour (Bishop _ c) = c
-colour (Knight _ c) = c
+colour (Pawn c _) = c
+colour (Rook c _) = c
+colour (King c _) = c
+colour (Queen c _) = c
+colour (Bishop c _) = c
+colour (Knight c _) = c
 colour Empty = T
+
+position :: Piece -> Pos
+position (Pawn _ p)   = p
+position (King _ p)   = p
+position (Rook _ p)   = p
+position (Queen _ p)  = p
+position (Bishop _ p) = p
+position (Knight _ p) = p
+
+mposition :: Move -> Pos
+mposition (Take p) = p
+mposition (Block p) = p
+mposition (Attack p) = p
+
+king :: Piece -> Bool
+king (King _ _) = True
+king _ = False
 
 left :: Colour -> Pos -> Pos
 left _ (x, y) = (x - 1, y)
@@ -104,10 +120,7 @@ opposite W = black
 opposite _ = (const False)
 
 lookAt:: Pos -> Board -> Maybe Piece
-lookAt p (Board ps _) = M.lookup p ps
-
-lookFor :: Piece -> Board -> Maybe Pos
-lookFor pc (Board _ pcs) = M.lookup pc pcs
+lookAt p (Board ps _ _) = M.lookup p ps
 
 attack :: Pos -> Colour -> Board -> Maybe Move
 attack pos colour = fmap (const (Attack pos)) . mfilter viable . lookAt pos
@@ -133,131 +146,136 @@ attackf f pos colour board = keepValid $ iterate (f colour) pos -- Do better, cr
 
 -- En Passant move missing
 -- Jumps shouldn't be allowed after jumping once
-pawnMoves :: Pos -> Colour -> Board -> [Move]
-pawnMoves p colour board = catMaybes [take' (leftUp colour p)  colour board, 
-                                      take' (rightUp colour p) colour board,
-                                      block (up colour p) board,
-                                      jump  (up colour (up colour p)) board]
+pawnMoves :: Piece -> Board -> [Move]
+pawnMoves (Pawn colour pos) board = catMaybes [take' (leftUp colour pos)  colour board, 
+                                               take' (rightUp colour pos) colour board,
+                                               block (up colour pos) board,
+                                               jump  (up colour (up colour pos)) board]
 
-kingMoves :: Pos -> Colour -> Board -> [Move]
-kingMoves p colour board = catMaybes [attack (leftUp colour p) colour board, 
-                                      attack (up colour p) colour board, 
-                                      attack (rightUp colour p) colour board, 
-                                      attack (right colour p) colour board,
-                                      attack (rightDown colour p) colour board,
-                                      attack (down colour p) colour board, 
-                                      attack (leftDown colour p) colour board, 
-                                      attack (left colour p) colour board,
-                                      Just QCastle,
-                                      Just KCastle]
+kingMoves :: Piece -> Board -> [Move]
+kingMoves (King colour pos) board = catMaybes [attack (leftUp colour pos) colour board, 
+                                               attack (up colour pos) colour board, 
+                                               attack (rightUp colour pos) colour board, 
+                                               attack (right colour pos) colour board,
+                                               attack (rightDown colour pos) colour board,
+                                               attack (down colour pos) colour board, 
+                                               attack (leftDown colour pos) colour board, 
+                                               attack (left colour pos) colour board,
+                                               Just QCastle,
+                                               Just KCastle]
 
-rookMoves :: Pos -> Colour -> Board -> [Move]
-rookMoves p colour board = (attackf up p colour board)    ++ 
-                           (attackf left p colour board)  ++
-                           (attackf right p colour board) ++
-                           (attackf down p colour board)
+rookMoves :: Piece -> Board -> [Move]
+rookMoves (Rook colour pos) board = (attackf up pos colour board)    ++ 
+                                    (attackf left pos colour board)  ++
+                                    (attackf right pos colour board) ++
+                                    (attackf down pos colour board)
 
-bishopMoves :: Pos -> Colour -> Board -> [Move]
-bishopMoves p colour board = (attackf leftUp p colour board)    ++
-                             (attackf rightUp p colour board)   ++
-                             (attackf leftDown p colour board)  ++
-                             (attackf rightDown p colour board)
+bishopMoves :: Piece -> Board -> [Move]
+bishopMoves (Bishop colour pos) board = (attackf leftUp pos colour board)    ++
+                                        (attackf rightUp pos colour board)   ++
+                                        (attackf leftDown pos colour board)  ++
+                                        (attackf rightDown pos colour board)
 
-queenMoves :: Pos -> Colour -> Board -> [Move]
-queenMoves p colour board = (rookMoves p colour board) ++ (bishopMoves p colour board)
+queenMoves :: Piece -> Board -> [Move]
+queenMoves (Queen colour pos) board = (rookMoves (Rook colour pos) board) ++ 
+                                      (bishopMoves (Bishop colour pos) board)
 
-knightMoves :: Pos -> Colour -> Board -> [Move]
-knightMoves p colour board = catMaybes [attack (right colour (up colour (up colour p))) colour board,
-                                        attack (right colour (down colour (down colour p))) colour board,
-                                        attack (left colour (down colour (down colour p))) colour board,
-                                        attack (left colour (up colour (up colour p))) colour board]
+knightMoves :: Piece -> Board -> [Move]
+knightMoves (Knight colour pos) board = catMaybes [attack (right colour (up colour (up colour pos))) colour board,
+                                                   attack (right colour (down colour (down colour pos))) colour board,
+                                                   attack (left colour (down colour (down colour pos))) colour board,
+                                                   attack (left colour (up colour (up colour pos))) colour board]
 
-moves :: Pos -> Piece -> Board -> [Move]
-moves pos (Pawn _ colour)   = pawnMoves pos colour
-moves pos (Rook _ colour)   = rookMoves pos colour
-moves pos (Bishop _ colour) = bishopMoves pos colour
-moves pos (Knight _ colour) = knightMoves pos colour 
-moves pos (Queen colour)    = queenMoves pos colour
-moves pos (King  colour)    = kingMoves pos colour
-moves pos Empty             = const []
+moves :: Piece -> Board -> [Move]
+moves piece @ (Pawn _ _)   = pawnMoves piece
+moves piece @ (Rook _ _)   = rookMoves piece
+moves piece @ (Bishop _ _) = bishopMoves piece
+moves piece @ (Knight _ _) = knightMoves piece 
+moves piece @ (Queen _ _)  = queenMoves piece
+moves piece @ (King _ _)   = kingMoves piece
+moves Empty                = const []
 
-mposition :: Move -> Pos
-mposition (Take p) = p
-mposition (Block p) = p
-mposition (Attack p) = p
-
-threatsOf :: Colour -> Board -> Set Pos
-threatsOf colour board = M.foldlWithKey gatherThreats S.empty $ filterByKey (opposite colour) $ pieces board
-        where gatherThreats set piece pos = set <> (S.fromList $ fmap mposition $ filter threats $ moves pos piece board)
+threatsFor :: Piece -> Board -> Set Pos
+threatsFor piece board = M.foldl gatherThreats S.empty $ M.filter (opposite $ colour piece) $ positions board
+        where gatherThreats set piece = set <> (S.fromList $ fmap mposition $ filter threats $ moves piece board)
               threats (Attack _) = True
               threats (Take _) = True
               threats _ = False
 
-check :: Pos -> Piece -> Move -> Board -> Maybe Move 
-check _ piece m board = fmap (const m) $ mfilter (not . inCheck) $ M.lookup king $ pieces board
-    where ocolour = invert $ colour piece
-          king = King ocolour
-          inCheck pos = S.member pos threats
-          threats = threatsOf ocolour board
+check :: Piece -> Move -> Board -> Maybe Move 
+check piece move board = if (not $ inCheck) then Just move else Nothing 
+    where kingPos = if (white piece)
+                    then whiteKing board 
+                    else blackKing board
+          inCheck = S.member kingPos $ threatsFor piece board
 
-checkmate :: Pos -> Piece -> Move -> Board -> Maybe Move
-checkmate pos piece m board = fmap (const m) $ mfilter (not . inCheckmate) $ M.lookup king $ pieces board
-    where ocolour = invert $ colour piece
-          king = King ocolour
-          inCheckmate pos = (inCheck pos) && (cannotMove pos) && unblockable
-          inCheck kingPos = S.member kingPos threats
-          cannotMove kingPos = S.isSubsetOf (kingMoves kingPos) threats
-          unblockable = isNothing $ check pos piece m $ apply pos m board 
-          kingMoves kingPos = S.fromList $ fmap mposition $ moves kingPos king board
-          threats = threatsOf ocolour board
+checkmate :: Piece -> Move -> Board -> Maybe Move
+checkmate piece move board = if (not $ inCheckmate) then Just move else Nothing
+    where king = if (white piece) 
+                 then King W (whiteKing board)
+                 else King B (blackKing board)
+          inCheckmate = inCheck && cannotMove && unblockable
+          inCheck     = S.member (position king) threats
+          cannotMove  = S.isSubsetOf kingMoves threats
+          unblockable = isNothing $ check piece move $ apply (position piece) move board 
+          kingMoves   = S.fromList $ fmap mposition $ moves king board
+          threats     = threatsFor piece board
 
-checked :: Pos -> Piece -> Move -> Board -> Maybe Move
-checked pos piece m board = check pos piece m $ apply pos m board 
+checked :: Piece -> Move -> Board -> Maybe Move
+checked piece m board = check piece m $ apply (position piece) m board 
 
 -- Stalemate check missing
 -- Chess total number of moves check missing
-checks :: Pos -> Piece -> Move -> Board -> Maybe Move -- maybe outcome
-checks pos piece move board = (check pos piece move board) <|> (checkmate pos piece move board)
+checks :: Piece -> Move -> Board -> Maybe Move -- maybe outcome
+checks piece move board = (check piece move board) <|> (checkmate piece move board)
 
-qcastle :: Pos -> Piece -> Move -> Board -> Maybe Move
-qcastle _ piece m board = fmap (const m) $ mfilter (const (not blocked)) $ (pure (&&) <*> hasKing <*> hasRook)
+-- castling is also colour dependent
+qcastle :: Piece -> Move -> Board -> Maybe Move
+qcastle piece m board = fmap (const m) $ mfilter (const $ not blocked) $ (pure (&&) <*> hasKing <*> hasRook)
     where c = colour piece
-          king p = p == (King c)
-          rook p = (p == (Rook 1 c)) || (p == (Rook 2 c))
+          king p = p == (King c queenCastleKing)
+          rook p = p == (Rook c queenCastleRookS)
           hasKing = fmap (const True) $ mfilter king $ lookAt queenCastleKing board
           hasRook = fmap (const True) $ mfilter rook $ lookAt queenCastleRookS board
           blocked = S.isSubsetOf safetyPath threats
-          safetyPath = S.fromList [(7, 1), (6, 1), (5, 1)]
-          threats = threatsOf (invert c) board        
+          threats = threatsFor piece board
+          safetyPath = S.fromList files
+          files = if (white piece) 
+                  then [(2, 1), (3, 1), (4, 1)]
+                  else [(2, 8), (3, 8), (4, 8)]
 
-kcastle :: Pos -> Piece -> Move -> Board -> Maybe Move
-kcastle _ piece m board = fmap (const m) $ mfilter (const (not blocked)) $ (pure (&&) <*> hasKing <*> hasRook)
-            where c = colour piece
-                  king p = p == (King c)
-                  rook p = (p == (Rook 1 c)) || (p == (Rook 2 c))
-                  hasKing = fmap (const True) $ mfilter king $ lookAt kingCastleKing board
-                  hasRook = fmap (const True) $ mfilter rook $ lookAt kingCastleRookS board
-                  blocked = S.isSubsetOf safetyPath threats
-                  safetyPath = S.fromList [(7, 1), (6, 1), (5, 1)]
-                  threats = threatsOf (invert c) board        
+kcastle :: Piece -> Move -> Board -> Maybe Move
+kcastle piece m board = fmap (const m) $ mfilter (const (not blocked)) $ (pure (&&) <*> hasKing <*> hasRook)
+    where c = colour piece
+          king p = p == (King c kingCastleKing)
+          rook p = p == (Rook c kingCastleRookS)
+          hasKing = fmap (const True) $ mfilter king $ lookAt kingCastleKing board
+          hasRook = fmap (const True) $ mfilter rook $ lookAt kingCastleRookS board
+          blocked = S.isSubsetOf safetyPath threats
+          threats = threatsFor piece board
+          safetyPath = S.fromList files
+          files = if (white piece) 
+                  then [(5, 1), (6, 1), (7, 1)]
+                  else [(5, 8), (6, 8), (7, 8)]   
 
-legality :: Pos -> Piece -> Move -> Board -> Maybe Move
-legality pos p m @ (Take pos') board   = (checks pos p m board) >?= (checked pos p m board)
-legality pos p m @ (Block pos') board  = (checks pos p m board) >?= (checked pos p m board)
-legality pos p m @ (Attack pos') board = (checks pos p m board) >?= (checked pos p m board)
-legality pos p m @ (Jump pos') board   = (checks pos p m board) >?= (checked pos p m board)
-legality pos p m @ QCastle board       = (checks pos p m board) >?= (checked pos p m board) >?= (qcastle pos p m board)
-legality pos p m @ KCastle board       = (checks pos p m board) >?= (checked pos p m board) >?= (kcastle pos p m board)
+legality :: Piece -> Move -> Board -> Maybe Move
+legality p m @ (Take pos') board   = (checks p m board) >?= (checked p m board)
+legality p m @ (Block pos') board  = (checks p m board) >?= (checked p m board)
+legality p m @ (Attack pos') board = (checks p m board) >?= (checked p m board)
+legality p m @ (Jump pos') board   = (checks p m board) >?= (checked p m board)
+legality p m @ QCastle board       = (checks p m board) >?= (checked p m board) >?= (qcastle p m board)
+legality p m @ KCastle board       = (checks p m board) >?= (checked p m board) >?= (kcastle p m board)
 
+-- TODO: This should change the king positions on the board if `piece` is one of the kings
 streamLine :: [(Pos, Pos)] -> Board -> Board
 streamLine moves board = foldl transform board moves
-    where transform board change = maybe board id (adapt board change)
-          adapt (Board ps pcs) (pos, pos') = do
-            piece  <- M.lookup pos  ps
-            piece' <- M.lookup pos' ps
-            let positions = M.insert pos' piece $ M.insert pos Empty ps
-            let pieces = M.delete piece' $ M.insert piece pos' pcs
-            return $ Board { positions = positions, pieces = pieces }
+          where transform board move = maybe board id $ adapt board move
+                adapt (Board ps wk bk) (pos, pos') = do
+                    piece  <- M.lookup pos ps
+                    let positions = M.insert pos' piece $ M.insert pos Empty ps
+                    let whiteKing = if (white piece && king piece) then pos' else wk
+                    let blackKing = if (black piece && king piece) then pos' else bk
+                    return $ Board { positions = positions, whiteKing = whiteKing, blackKing = blackKing }
 
 apply :: Pos -> Move -> Board -> Board
 apply pos (Take pos')   = streamLine [(pos, pos')]
@@ -271,18 +289,23 @@ move :: Pos -> Move -> Board -> Board
 move pos m board = fromMaybe board newBoard
     where newBoard = do
             piece <- lookAt pos board 
-            _     <- find (== m) $ moves pos piece board
-            _     <- legality pos piece m board
+            _     <- find (== m) $ moves piece board
+            _     <- legality piece m board
             return (apply pos m board)
 
 board :: Board 
-board = Board { positions = M.fromList ps, pieces = M.fromList pcs }
-    where pawns c y = fmap (\x -> ((x, y), Pawn x c)) [1..8]
-          pieces c y = zipWith (\x p -> ((x, y), p)) [1..8] (row c)
-          row c = [Rook 1 c, Knight 1 c, Bishop 1 c, Queen c, King c, Bishop 2 c, Knight 2 c, Rook 2 c]
+board = Board { positions = M.fromList positions, 
+                whiteKing = (5, 1), 
+                blackKing = (5, 8) }
+    where row c y = zipWith (\x f -> ((x, y), f c (x, y))) [1..8]
           empties = [((x, y), Empty) | x <- [1..8], y <- [3..6]]
-          ps  = (pawns B 7) ++ (pieces B 8) ++ (pawns W 2) ++ (pieces W 1) ++ empties
-          pcs = fmap swap ps
+          pawns   = [Pawn, Pawn,   Pawn,   Pawn,  Pawn, Pawn,   Pawn,   Pawn]
+          pieces  = [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook]
+          positions  = (row B 8 pieces) ++
+                       (row B 7 pawns) ++  
+                        empties ++ 
+                       (row W 2 pawns) ++ 
+                       (row W 1 pieces)
 
 
 instance Show Board where
@@ -290,18 +313,18 @@ instance Show Board where
         where row y = foldl (++) "" $ intersperse "," $ catMaybes $ fmap (\x -> fmap show $ lookAt (x, y) board) [1..8]
 
 instance Show Piece where
-    show (Pawn _ W)   = " ♙ "
-    show (Pawn _ B)   = " ♟ "
-    show (Rook _ W)   = " ♖ "
-    show (Rook _ B)   = " ♜ "
-    show (Bishop _ W) = " ♗ "
-    show (Bishop _ B) = " ♝ "
-    show (Knight _ W) = " ♘ "
-    show (Knight _ B) = " ♞ "
-    show (Queen W)    = " ♕ "
-    show (Queen B)    = " ♛ "
-    show (King W)     = " ♔ "
-    show (King B)     = " ♚ "
+    show (Pawn W _)   = " ♙ "
+    show (Pawn B _)   = " ♟ "
+    show (Rook W _)   = " ♖ "
+    show (Rook B _)   = " ♜ "
+    show (Bishop W _) = " ♗ "
+    show (Bishop B _) = " ♝ "
+    show (Knight W _) = " ♘ "
+    show (Knight B _) = " ♞ "
+    show (Queen W _)  = " ♕ "
+    show (Queen B _)  = " ♛ "
+    show (King W _)   = " ♔ "
+    show (King B _)   = " ♚ "
     show Empty        = "   "
 
 --- Improvements: 
