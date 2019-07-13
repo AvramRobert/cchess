3bfb2a753ee1b3a78935a3ec4dbd80baf6f32799
Robert M. Avram <robert.avram@rewe-digital.com>
2019-07-13 12:24:12 +0200

    Separate Chess in internal and api

--- src/Chess.hs
+++ src/Chess.hs
@@ -1,6 +1,16 @@
 module Chess where 
 
-import Chess.Internal as Chess
+import qualified Chess.Internal as Chess
+import Data.Set (Set)
 
 newBoard :: Chess.Board
 newBoard = Chess.board
+
+applyMove :: Chess.Move -> Chess.Board -> Either Chess.Outcome Chess.Board
+applyMove m b = Chess.move m b
+
+legalMoves :: Chess.Board -> Set Chess.Move
+legalMoves = Chess.availableMoves 
+
+currentPlayer :: Chess.Board -> Chess.Colour
+currentPlayer = Chess.player
