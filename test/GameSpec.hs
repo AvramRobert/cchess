5467a1f29f1a7c3253384ae3e1a90b4f13161211
Robert M. Avram <robert.avram@rewe-digital.com>
2019-07-29 00:15:05 +0200

    Add unit test for all specs

--- test/GameSpec.hs
+++ test/GameSpec.hs
@@ -1,9 +1,28 @@
 module GameSpec (pgnFileSpec) where
 
 import Test.Hspec
+import qualified PGN as P
+import System.IO.Unsafe (unsafePerformIO)
+import Data.List (all)
+import Data.Either (isRight)
+
+data Result = Game String deriving (Show)
+
+pgnGames :: [String]
+pgnGames = unsafePerformIO $ P.fromFile' "./test/resources/chess_games/2019_game_batch_red.pgn" 
+
+computeGames :: [String] -> IO ()
+computeGames = computeFrom 1 
+    where computeFrom _ []     = putStrLn "Successfully finished!"
+          computeFrom i (x:xs) =  do
+            let computed = P.compute x
+            _ <- putStrLn $ "Computing game " <> (show i)
+            if (isRight computed)
+            then computeFrom (i + 1) xs
+            else error $ "Failed with game: " <> (show $ Game x)
 
 pgnFileSpec :: Spec
 pgnFileSpec = do
     describe "PGN file games" $ do
         it "can be parsed and played" $ do
-            True `shouldBe` True
+            computeGames pgnGames
