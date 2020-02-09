07913055f2ff6e8fd2954a79d6f28831c3779046
Robert Avram <am11.robert@yahoo.com>
2020-02-09 18:32:26 +0100

    Fleshing new design idea

--- src/Chess/Internal2.hs
+++ src/Chess/Internal2.hs
@@ -17,26 +17,64 @@ module Chess.Internal2 where
 
 -- castling, passants and checks -> keep them at a game level, not at a piece level.
 
-type Square = (Integer, Integer)
-data Colour = B | W
+type Square = (Int, Int)
+data Colour = B | W deriving (Eq, Show)
 
 
-data Action = Capture | Advance
-data Piece = Pawn | Knight | Bishop | Rook | Queen | King
-data Devel = Devel Piece Colour Action [Square] -- the piece, the action and the list of squares he visits. Last being the one where the action occurs -> more optimal: make it the head
+data Action = Capture | Advance deriving (Eq, Show)
+data Piece = Pawn | Knight | Bishop | Rook | Queen | King deriving (Eq, Show)
+data Development = Development Action Piece Colour [Square] deriving (Eq, Show)
+-- the piece, the action and the list of squares he visits. Last being the one where the action occurs -> more optimal: make it the head
 
-type Move = [Devel]
+data Move = Single Development |
+            Compound [Development]
+
+data Dir = U  | D  | L  | R |
+           UL | UR | DL | DR deriving (Show, Eq)
 
 data Board = Board { check  :: Bool,
                      player :: Colour,
-                     king   :: (Colour, Square),
                      pieces :: [(Piece, Colour, Square)]
-                     }
+                     } deriving (Eq, Show)
 
-spread :: [a -> b] -> [a] -> [b]
-spread fs as = [f a | a <- as, f <- fs] 
+spread :: [a -> b] -> a -> [b]
+spread fs a = [f a | f <- fs] 
 
-pawnMoves :: Colour -> Board -> Move
-pawnMoves colour = spread [] . filter (pawns colour) . pieces
-    where pawns W (Pawn, W, _) = True
-          pawns B (Pawn, B, _) = False
+develop :: [Dir] -> (Piece, Colour, Square) -> (Piece, Colour, [Square]) 
+develop dir (piece, colour, square) = (piece, colour, scanl (develop' colour) square dir)
+    where develop' W (x, y) L  = (x - 1, y)
+          develop' W (x, y) R  = (x + 1, y)
+          develop' W (x, y) U  = (x, y + 1)
+          develop' W (x, y) D  = (x, y - 1)
+          develop' W (x, y) UL = (x - 1, y + 1)
+          develop' W (x, y) UR = (x + 1, y + 1)
+          develop' W (x, y) DL = (x - 1, y - 1)
+          develop' W (x, y) DR = (x + 1, y - 1)
+          develop' B (x, y) L  = (x - 1, y)
+          develop' B (x, y) R  = (x + 1, y)
+          develop' B (x, y) U  = (x, y - 1)
+          develop' B (x, y) D  = (x, y + 1)
+          develop' B (x, y) UL = (x - 1, y - 1)
+          develop' B (x, y) UR = (x + 1, y - 1)
+          develop' B (x, y) DL = (x - 1, y + 1)
+          develop' B (x, y) DR = (x + 1, y + 1)
+
+advance :: [Dir] -> (Piece, Colour, Square) -> Development
+advance dir = create . develop dir
+    where create (piece, colour, squares) = Development Advance piece colour squares
+
+capture :: [Dir] -> (Piece, Colour, Square) -> Development
+capture dir = create . develop dir
+    where create (piece, colour, squares) = Development Capture piece colour squares
+
+verify :: Action -> Board -> Move -> Maybe Move
+verify Capture board (Single (Development Capture piece colour squares)) = Nothing -- this might actually work
+
+-- in order to keep it rather abstract at the usage level
+-- I have to check the validity of a move a the point where it is defined, not in a separate function
+-- If I do this, then only the local details of every move definition will contain specialised code, the usage functions and everything else will be kept generalised
+pawnMoves :: (Piece, Colour, Square) -> [Move]
+pawnMoves = spread [Single . capture [UL],
+                    Single . capture [UR],
+                    Single . advance [U],
+                    Single . advance [U, U]]
