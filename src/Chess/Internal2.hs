32351f220da00ffe5f9b085ac5f824d923834d37
Robert Avram <robert.avram@rewe-digital.com>
2020-02-11 16:55:10 +0100

    New idea

--- src/Chess/Internal2.hs
+++ src/Chess/Internal2.hs
@@ -1,5 +1,9 @@
 module Chess.Internal2 where
 
+import Data.List (find)
+import Control.Monad (mfilter)
+import Data.Functor ((<$))
+import Data.Maybe (maybe)
 
 -- What do I need to be able to do?
 
@@ -20,14 +24,14 @@ module Chess.Internal2 where
 type Square = (Int, Int)
 data Colour = B | W deriving (Eq, Show)
 
-
-data Action = Capture | Advance deriving (Eq, Show)
 data Piece = Pawn | Knight | Bishop | Rook | Queen | King deriving (Eq, Show)
-data Development = Development Action Piece Colour [Square] deriving (Eq, Show)
--- the piece, the action and the list of squares he visits. Last being the one where the action occurs -> more optimal: make it the head
+data Action = Action Piece Colour [Square] deriving (Eq, Show) -- squares should always be in reverse order of action => head is the last square
 
-data Move = Single Development |
-            Compound [Development]
+data Move = Capture Action   |
+            Advance Action   |
+            Promote Action   |
+            Castle [Action]   -- this is the only thing that is sort-of compound
+            deriving (Eq, Show) 
 
 data Dir = U  | D  | L  | R |
            UL | UR | DL | DR deriving (Show, Eq)
@@ -59,22 +63,31 @@ develop dir (piece, colour, square) = (piece, colour, scanl (develop' colour) sq
           develop' B (x, y) DL = (x - 1, y + 1)
           develop' B (x, y) DR = (x + 1, y + 1)
 
-advance :: [Dir] -> (Piece, Colour, Square) -> Development
+colour :: (Piece, Colour, Square) -> Colour
+colour (_, c, _) = c
+
+lookAt :: Square -> Board -> Maybe (Piece, Colour, Square)
+lookAt square = find position . pieces
+    where position (_, _, square') = square == square'
+
+advance :: [Dir] -> (Piece, Colour, Square) -> Move
 advance dir = create . develop dir
-    where create (piece, colour, squares) = Development Advance piece colour squares
+    where create (piece, colour, squares) = Advance (Action piece colour squares)
 
-capture :: [Dir] -> (Piece, Colour, Square) -> Development
+capture :: [Dir] -> (Piece, Colour, Square) -> Move
 capture dir = create . develop dir
-    where create (piece, colour, squares) = Development Capture piece colour squares
+    where create (piece, colour, squares) = Capture (Action piece colour squares)
 
-verify :: Action -> Board -> Move -> Maybe Move
-verify Capture board (Single (Development Capture piece colour squares)) = Nothing -- this might actually work
+verify :: Move -> Board -> Maybe Move
+verify m @ (Capture (Action piece colour' squares))                                   = Just m <$ mfilter ((/= colour') . colour) . lookAt (head squares) -- this might actually work
+verify m @ (Advance (Action piece colour' squares))                                   = maybe (Just m) (const Nothing) . lookAt (head squares)
+verify m @ (Castle  [(Action King kcolour ksquares), (Action Rook rcolour rsquares)]) = const Nothing
 
 -- in order to keep it rather abstract at the usage level
 -- I have to check the validity of a move a the point where it is defined, not in a separate function
 -- If I do this, then only the local details of every move definition will contain specialised code, the usage functions and everything else will be kept generalised
 pawnMoves :: (Piece, Colour, Square) -> [Move]
-pawnMoves = spread [Single . capture [UL],
-                    Single . capture [UR],
-                    Single . advance [U],
-                    Single . advance [U, U]]
+pawnMoves = spread [capture [UL],
+                    capture [UR],
+                    advance [U],
+                    advance [U, U]]
