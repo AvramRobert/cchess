1558f085f44558794d26ac5bca7fbdd73bcd7b7b
Robert Avram <robert.avram@rewe-digital.com>
2020-04-20 00:40:19 +0200

    Freer..

--- app/Game.hs
+++ app/Game.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE GADTs #-}
+
 module Game (runGame) where
 
+import Data.Function (flip)
 import Control.Monad (foldM)
-import Control.Applicative ((<|>))
+import Control.Applicative ((<|>), (<$>))
 import qualified Chess.Display as D
 import qualified Chess.Internal as C
 import qualified Text.Megaparsec as M
@@ -19,10 +22,43 @@ data State = Menu               |
              Resign Game        |
              End Game C.Outcome |
              Exit 
-               
-data Instruction = Display String                   | 
-                   Interact (P.Parser Instruction)  | 
-                   Transition State                 
+
+data Act f a where
+    Value :: a -> Act f a
+    Effect :: (f x) -> (x -> Act f a) -> Act f a
+
+instance Functor (Act f) where
+    fmap f (Value a)     = Value $ f a
+    fmap f (Effect fx g) = Effect fx (fmap f . g) 
+
+instance Applicative (Act f) where
+    pure = Value
+    (<*>) (Value f) (Value a)             = Value $ f a
+    (<*>) fab (Effect fx f)               = Effect fx (\x -> fab <*> (f x))
+    (<*>) (Effect fx f) fa                = Effect fx (\x -> (f x) <*> fa)
+
+instance Monad (Act f) where
+    (>>=) (Value a) f      = f a
+    (>>=) (Effect fx ff) f = Effect fx (\x -> (ff x) >>= f) 
+
+data Prompt a where
+    Display :: String     -> Prompt String
+    Input   :: P.Parser a -> Prompt a
+    Stop    :: Prompt a
+
+type Instruction a = Act Prompt a
+
+act :: f a -> Act f a
+act f = Effect f Value
+
+display :: String -> Instruction String
+display = act . Display
+
+input :: P.Parser State -> Instruction State
+input = act . Input
+
+stop :: Instruction State
+stop = act Stop
 
 showBoard :: Game -> String
 showBoard = D.showBoard D.GameMode . board
@@ -57,43 +93,42 @@ resignText c = unlines ["", "Result: (W) " <> w <> " -" <> b <> " (B)"]
 exitText :: String
 exitText = "One day at a time."
 
-newGameParser :: P.Parser Instruction
-newGameParser = MC.string' "new game" $> (Transition $ Play Game { board = C.board, white = "Whitney", black = "Clareance" })
+newGame :: P.Parser State
+newGame = MC.string' "new game" $> Play Game { board = C.board, white = "Whitney", black = "Clareance" }
 
-moveParser :: Game -> P.Parser Instruction
-moveParser game = fmap (Transition . handle . C.performEval (board game)) $ P.moveParser (board game)
+move :: Game -> P.Parser State
+move game = fmap (handle . C.performEval (board game)) $ P.moveParser (board game)
     where handle (Right board)  = Play game { board = board }
           handle (Left outcome) = End game outcome
 
-exitGameParser :: P.Parser Instruction
-exitGameParser = M.choice [ MC.string' "exit", MC.string' "quit" ] $> (Transition Exit)
+exit :: P.Parser State
+exit = M.choice [ MC.string' "exit", MC.string' "quit" ] $> Exit
 
-resignParser :: Game -> P.Parser Instruction
-resignParser game = M.choice [ MC.string' "resign", MC.string' "exit"] $> (Transition (Resign game))
+resign :: Game -> P.Parser State
+resign game = M.choice [ MC.string' "resign", MC.string' "exit"] $> (Resign game)
 
-instructions :: State -> [Instruction]
-instructions (Menu)              = [Display menuText, Interact (newGameParser <|> exitGameParser)]
-instructions (Exit)              = [Display exitText]
-instructions (Play game)         = [Display (playText game), Interact (moveParser game <|> resignParser game)]
-instructions (End game outcome)  = [Display (outcomeText outcome)]
-instructions (Resign game)       = [Display (resignText $ C.player $ board game)]
+instructions :: State -> Instruction State
+instructions (Menu)              = display menuText >> input (newGame <|> exit)
+instructions (Exit)              = display exitText >> stop
+instructions (Play game)         = display (playText game) >> input (move game <|> resign game)
+instructions (End game outcome)  = display (outcomeText outcome) >> stop
+instructions (Resign game)       = display (resignText $ C.player $ board game) >> stop
 
-process :: Instruction -> IO ()
-process (Display text)     = putStrLn text
-process (Transition state) = run state 
-process (Interact p)       = getLine >>= (handle . P.run p)
-    where handle (Right i)            = process i
-          handle (Left err)           = maybe (unknown err) known $ P.chessError err
-          unknown err                 = process (Display $ "Unknown input. Try again\n") >> process (Interact p)  
-          known (P.MissingMovesError) = process (Display $ "Unknown move. Try again") >> process (Interact p)
-          known (P.CaptureError c f)  = process (Display $ "Cannot capture with " <> showFigure f <> " at " <> show c) >> process (Interact p) 
-          known (P.AdvanceError c f)  = process (Display $ "Cannot advance to " <> showFigure f <> " with " <> show pi) >> process (Interact p)
-          known (P.PromoteError c f)  = process (Display $ "Cannot promote to " <> showFigure f <> " at " <> show c) >> process (Interact p)
-          known (P.CastleError c)     = process (Display $ "Cannot castle" <> show c) >> process (Interact p)
-          known (P.GameError o)       = process (Display $ "Cannot perform because the game is: " <> show o) >> process (Interact p)
+process :: Instruction State -> IO ()
+process (Value s)                     = process $ instructions s
+process (Effect (Stop) _)             = return ()
+process (Effect (Display s) f)        = putStrLn s >> (process $ f s)
+process eff @ (Effect (Input p) f)    = getLine >>= (handle . P.run p)
+    where handle (Right state)        = process $ f state
+          handle (Left error)         = maybe (unknown error) (known) (P.chessError error) >> process eff
+          unknown error               = putStrLn $ "Unknown input. Try again\n"
+          known (P.MissingMovesError) = putStrLn $ "Unknown move. Try again"
+          known (P.CaptureError c f)  = putStrLn $ "Cannot capture with " <> showFigure f <> " at " <> show c 
+          known (P.AdvanceError c f)  = putStrLn $ "Cannot advance to " <> showFigure f <> " with " <> show pi 
+          known (P.PromoteError c f)  = putStrLn $ "Cannot promote to " <> showFigure f <> " at " <> show c
+          known (P.CastleError c)     = putStrLn $ "Cannot castle" <> show c 
+          known (P.GameError o)       = putStrLn $ "Cannot perform because the game is: " <> show o
 
-run :: State -> IO ()
-run = foldM (\_ i -> process i) () . instructions
 
 runGame :: IO ()
-runGame = run Menu
+runGame = process $ instructions Menu
